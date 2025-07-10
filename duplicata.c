4c9a77d778d94ab61df353b0e821ee72df9a94df
Rafael <josepatrickhotmail710@gmail.com>
2025-07-10 16:57:05 -0300

    agora implementamos as definicões duas estruturas de dados importantes para a implementação de uma tabela hash

--- duplicata.c
+++ duplicata.c
@@ -7,3 +7,21 @@ typedef unsigned long (*HashFunc)(void* key);
 typedef int (*CmpFunc)(void* a, void* b);
 typedef void (*FreeFunc)(void* data);
 typedef void (*PrintFunc)(void* data);
+
+typedef struct Entry {
+    void* key;
+    void* value;
+    struct Entry* next;
+} Entry;
+
+typedef struct HashTable {
+    Entry** buckets;
+    int size;
+    int count;
+    int collisions;
+
+    HashFunc hash;
+    CmpFunc compare;
+    FreeFunc freeKey;
+    FreeFunc freeVal;
+} HashTable;
